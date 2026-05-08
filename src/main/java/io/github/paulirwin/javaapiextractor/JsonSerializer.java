172e699978369eba1fa8ffd70384a363b8e00f9b
Paul Irwin <paulirwin@gmail.com>
2026-05-08 10:23:57 -0600

    Upgrade Maven packages; exclude test libraries from target

--- src/main/java/io/github/paulirwin/javaapiextractor/JsonSerializer.java
+++ src/main/java/io/github/paulirwin/javaapiextractor/JsonSerializer.java
@@ -1,13 +1,14 @@
 package io.github.paulirwin.javaapiextractor;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
 
 public class JsonSerializer {
-    public static String serialize(Object obj) {
-        var objectMapper = new ObjectMapper();
+    private static final ObjectMapper OBJECT_MAPPER = JsonMapper.builder().build();
 
+    public static String serialize(Object obj) {
         try {
-            return objectMapper.writeValueAsString(obj);
+            return OBJECT_MAPPER.writeValueAsString(obj);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
